a729e1019b2b39d2179afdd7e2deb6cca322a474
IHerelis <agiresubito@gmail.com>
2025-04-10 23:26:59 +0300

    add planets name inscription

--- src/components/parade-planets/Game-item.jsx
+++ src/components/parade-planets/Game-item.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 const GameItem = ({planetItem, startDrag, index, dropped, dragOver, dragLeave, dragEnd}) => {
+  
+  // const nameFanLetters = (value) => {
+  //   const lettersList = document.querySelectorAll(".item__name");
+
+  //   let deg = 0;
+  //   for (let item of lettersList) {
+  //     item.style.transform = `rotate(${deg}deg)`;
+  //     deg += value;
+  //   }
+
+  //   return value;
+  // }
+  // nameFanLetters(50);
 
   return (
     <div className='field__item' 
@@ -8,6 +21,14 @@ const GameItem = ({planetItem, startDrag, index, dropped, dragOver, dragLeave, d
     onDragStart={startDrag} draggable="true" data-index={index} data-name={planetItem.name}
     onDrop={dropped} onDragOver={dragOver} onDragLeave={dragLeave} onDragEnd={dragEnd}
     >
+      <div className='item__name'>
+        <div className='item__name__content'>{planetItem.name}</div>
+        {/* {
+          planetItem.name.split("").map((item, index) => {
+            return  <div key={index} className='item__name__content'>{item}</div>
+          })
+        } */}
+      </div>
       
     </div>
   );
