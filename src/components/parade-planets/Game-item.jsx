b7d89fced10859f58d95d053ee27c74af3adaea7
IHerelis <agiresubito@gmail.com>
2025-04-12 19:50:57 +0300

    made the name text a wheel

--- src/components/parade-planets/Game-item.jsx
+++ src/components/parade-planets/Game-item.jsx
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const GameItem = ({planetItem, startDrag, index, dropped, dragOver, dragLeave, dragEnd}) => {
   
-  // const nameFanLetters = (value) => {
-  //   const lettersList = document.querySelectorAll(".item__name");
+  const itemNameBlock = useRef();
 
-  //   let deg = 0;
-  //   for (let item of lettersList) {
-  //     item.style.transform = `rotate(${deg}deg)`;
-  //     deg += value;
-  //   }
+  useEffect(() => {  
+    nameFanLetters(8);
 
-  //   return value;
-  // }
-  // nameFanLetters(50);
+  }, [planetItem]);
+  
+
+
+  const nameFanLetters = (value) => {
+    const lettersList = itemNameBlock.current.querySelectorAll(".item__name__content");
+
+    itemNameBlock.current.style.transform = `rotate(${(value * lettersList.length) / 2 - value}deg)`;
+
+    let deg = 0;
+    for (let item of lettersList) {
+      item.style.transform = `rotate(${deg}deg)`;
+      deg -= value;
+    }
+
+    return value;
+  }
+
 
   return (
-    <div className='field__item' 
+    <div className='field__item'
     style={{background: `url(${planetItem.visualView}) center/cover no-repeat`}}
     onDragStart={startDrag} draggable="true" data-index={index} data-name={planetItem.name}
     onDrop={dropped} onDragOver={dragOver} onDragLeave={dragLeave} onDragEnd={dragEnd}
     >
-      <div className='item__name'>
-        <div className='item__name__content'>{planetItem.name}</div>
-        {/* {
+      <div ref={itemNameBlock} className='item__name'>
+        {/* <div className='item__name__content'>{planetItem.name}</div> */}
+        {
           planetItem.name.split("").map((item, index) => {
-            return  <div key={index} className='item__name__content'>{item}</div>
+            return  <div key={index} className='item__name__content'>
+              <div className='item__name__letter'>
+                {item}
+              </div>
+            </div>
           })
-        } */}
+        }
       </div>
       
     </div>
